c06550421a2fbb7ccb71a515de29e06cf52afc9f
Sebastian Haesselbarth <seb@sebmail.de>
2023-12-19 16:27:03 +0100

    Add adapter-config.d.ts

---
+++ src/lib/adapter-config.d.ts
@@ -0,0 +1,19 @@
+// This file extends the AdapterConfig type from "@types/iobroker"
+
+// Augment the globally declared type ioBroker.AdapterConfig
+declare global {
+    namespace ioBroker {
+        interface AdapterConfig {
+            AuthCode: string;
+            CallbackURL: string;
+            Configured: boolean;
+            Identifier: string;
+            Interval: number;
+            Language: string;
+            Secret: string;
+        }
+    }
+}
+
+// this is required so the above AdapterConfig is found by TypeScript / type checking
+export {};
