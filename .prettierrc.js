183c8638f6eef788bd330898e7b0232b63afc0b5
Sebastian Haesselbarth <seb@sebmail.de>
2024-07-06 15:49:40 +0200

    Prettier: Change max width to 180 chars

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     semi: true,
     trailingComma: 'all',
     singleQuote: true,
-    printWidth: 200,
+    printWidth: 180,
     useTabs: false,
     tabWidth: 4,
     endOfLine: 'lf',
